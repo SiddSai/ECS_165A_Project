7d26fb25f3721801a366a44672261800f9d7f5af
Siddharth Sai <2004siddharthsai@gmail.com>
2026-02-10 13:46:02 -0800

    db and index done

--- 165a-winter-2026-main/lstore/db.py
+++ 165a-winter-2026-main/lstore/db.py
@@ -20,7 +20,10 @@ class Database():
     :param key: int             #Index of table key in columns
     """
     def create_table(self, name, num_columns, key_index):
+        if table := self.get_table(name):
+            raise Exception(f"Warning: Table named {name} already exists")
         table = Table(name, num_columns, key_index)
+        self.tables.append(table)
         return table
 
     
@@ -28,11 +31,17 @@ class Database():
     # Deletes the specified table
     """
     def drop_table(self, name):
-        pass
+        if table := self.get_table(name):
+            self.tables.remove(table)
+        else:
+            raise Exception(f"Warning: Table named {name} does not exist")
 
     
     """
     # Returns table with the passed name
     """
     def get_table(self, name):
-        pass
+        for table in self.tables:
+            if table.name == name:
+                return table
+        return None
--- 165a-winter-2026-main/lstore/index.py
+++ 165a-winter-2026-main/lstore/index.py
@@ -7,32 +7,44 @@ class Index:
     def __init__(self, table):
         # One index for each table. All our empty initially.
         self.indices = [None] *  table.num_columns
-        pass
+        self.table = table
+
+        # Create indexes in key column
+        self.indices[table.key] = {}
 
     """
     # returns the location of all records with the given value on column "column"
     """
 
     def locate(self, column, value):
-        pass
+        # Check if does not exist
+        if self.indices[column] is None:
+            return []
+        return self.indices[column].get(value, [])
 
     """
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
     """
 
     def locate_range(self, begin, end, column):
-        pass
+        # Check if does not exist
+        if self.indices[column] is None:
+            return []
+        result = []
+        for value in self.indices[column]:
+            if begin <= value <= end:
+                result.extend(self.indices[column][value])
 
     """
     # optional: Create index on specific column
     """
 
     def create_index(self, column_number):
-        pass
+        self.indices[column_number] = {}
 
     """
     # optional: Drop index of specific column
     """
 
     def drop_index(self, column_number):
-        pass
+        self.indices[column_number] = None
