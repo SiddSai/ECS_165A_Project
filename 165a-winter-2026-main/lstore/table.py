93f2a50ce6b3ec9cf6e8274ae26621d5d29aaea6
Lucas Lam <lucas.lam006@gmail.com>
2026-02-10 20:44:53 -0800

    table: init, insert, read done

--- 165a-winter-2026-main/lstore/table.py
+++ 165a-winter-2026-main/lstore/table.py
@@ -1,10 +1,17 @@
 from lstore.index import Index
+from lstore.page import Page
 from time import time
+
+"""
+The Table class provides the core of our relational storage functionality. All columns are 64-bit integers in this implementation. Users mainly interact with tables through queries. Tables provide a logical view of the actual physically stored data and mostly manage the storage and retrieval of data. Each table is responsible for managing its pages and requires an internal page directory that, given a RID, returns the actual physical location of the record. The table class should also manage the periodical merge of its corresponding page ranges.
+"""
 
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
 TIMESTAMP_COLUMN = 2
 SCHEMA_ENCODING_COLUMN = 3
+NULL_RID = -1
+
 
 
 class Record:
@@ -14,6 +21,7 @@ class Record:
         self.key = key
         self.columns = columns
 
+# need page ranges
 class Table:
 
     """
@@ -25,11 +33,95 @@ class Table:
         self.name = name
         self.key = key
         self.num_columns = num_columns
+
         self.page_directory = {}
         self.index = Index(self)
         self.merge_threshold_pages = 50  # The threshold to trigger a merge
+
+        self.next_rid = 0
+        self.next_tail_rid = 0
+        self.page_ranges = []
+
+        self.base_pages = []
+        self.tail_pages = []
         pass
 
+    """
+    if time: skeleton might expect insert to return True/False for success (method currently ends with pass). replace with a return (dependent on query)
+    """
+    def insert(self, record):
+        # generate new rid
+        rid = self.next_rid
+        self.next_rid += 1
+
+        # if no pages exist or current page is full, create new page
+        if len(self.base_pages) == 0:
+            bundle = [Page() for col in range(self.num_columns + 4)]
+            self.base_pages.append(bundle)
+        current = self.base_pages[-1]
+        rid_page = current[RID_COLUMN]
+
+        if not rid_page.has_capacity():
+            bundle = [Page() for col in range(self.num_columns + 4)]
+            self.base_pages.append(bundle)
+            current = self.base_pages[-1]
+            rid_page = current[RID_COLUMN]
+        
+        current[INDIRECTION_COLUMN].write(NULL_RID) # null for base page
+        offset = rid_page.write(rid)
+        current[TIMESTAMP_COLUMN].write(int(time()))
+        current[SCHEMA_ENCODING_COLUMN].write(0) # no columns updated
+        for col in range(self.num_columns):
+            current[col + 4].write(record.columns[col])
+        
+        # update page directory with new rid and location
+        page_id = len(self.base_pages) - 1
+        range_id = 0 # until page ranges are implemented
+        is_tail = False
+
+        self.page_directory[rid] = (range_id, is_tail, page_id, offset)
+
+        # update index with new rid and key value
+        key_value = record.columns[self.key]
+        self.index.insert(key_value, rid) # no insert method yet for index?
+        pass
+
+    # future: implement read_latest()
+    def read(self, rid):
+        # Retrieve record with the given RID by looking up the page directory and reading from the appropriate page
+        if rid not in self.page_directory:
+            return None
+        range_id, is_tail, page_id, offset = self.page_directory[rid]
+        if is_tail:
+            bundle = self.tail_pages[page_id]
+        else:
+            bundle = self.base_pages[page_id]
+        # metadata
+        indirection = bundle[INDIRECTION_COLUMN].read(offset)
+        stored_rid = bundle[RID_COLUMN].read(offset)
+        timestamp = bundle[TIMESTAMP_COLUMN].read(offset)
+        schema = bundle[SCHEMA_ENCODING_COLUMN].read(offset)
+        # user columns
+        columns = []
+        for col in range(self.num_columns):
+            value = bundle[col + 4].read(offset)
+            columns.append(value)
+        key_value = columns[self.key]
+
+        return Record(rid, key_value, columns)
+        
+
+    def update(self, primary_key, updated_columns):
+        # Update the record with the given RID by writing the updated values to a new tail page and updating the page directory and index accordingly.
+        if len(columns) != self.num_columns:
+            return False
+        pass
+
+    def delete(self, rid):
+        # Mark the record with the given RID as deleted by updating the appropriate page and page directory entry.
+        pass
+
+    # do last
     def __merge(self):
         print("merge is happening")
         pass
